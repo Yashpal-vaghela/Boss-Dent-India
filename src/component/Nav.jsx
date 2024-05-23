e6248a4cde8f0af73ca30086d5b4aaef2334aed9
deepramani05 <135039433+deepramani05@users.noreply.github.com>
2024-05-23 10:12:39 +0530

    Navbar

--- src/component/Nav.jsx
+++ src/component/Nav.jsx
@@ -7,6 +7,7 @@ import { FaUserAlt } from "react-icons/fa";
 import { FaHeart } from "react-icons/fa6";
 import { FaCartPlus } from "react-icons/fa";
 import { FaSearch } from "react-icons/fa";
+import { FaPhoneAlt } from "react-icons/fa";
 
 const Nav = () => {
   return (
@@ -75,6 +76,42 @@ const Nav = () => {
               </div>
               <div className="main-nav-price-icon">
                 <p>$ 0.00</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="menu-main">
+          <div className="menu-sub">
+            <div className="menu-div">
+              <ul>
+                <li>
+                  <Link>HOME</Link>
+                </li>
+                <li>
+                  <Link>SHOP</Link>
+                </li>
+                <li>
+                  <Link>ABOUT US</Link>
+                </li>
+                <li>
+                  <Link>MY ACCOUNT</Link>
+                </li>
+                <li>
+                  <Link>CONTACT</Link>
+                </li>
+              </ul>
+            </div>
+            <div className="menu-num-main">
+              <div className="menu-num-sub">
+                <div className="menu-num-icon">
+                  <span>
+                    <FaPhoneAlt />
+                  </span>
+                </div>
+                <div className="menu-num-txt">
+                  <p>Need any Help ?</p>
+                  <Link>+91 76988 28883</Link>
+                </div>
               </div>
             </div>
           </div>
