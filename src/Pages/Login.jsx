846a5dd8d2b215ff35f3f0358e2c79245cd22b24
Yashpal-vaghela <vaghela9632@gmail.com>
2024-06-13 19:30:03 +0530

    loginpage set

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,12 +1,20 @@
 import React, { useState } from 'react'
+import { FaEyeSlash, FaEye } from 'react-icons/fa'
 
 const Login = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const [showPassword, setShowPassword] = useState(false)
 
     const handleSubmit = (e) =>{
         e.preventDefault()
-        console.log(email, password)
+        console.log("email",email, "password",password)
+        setEmail('')
+        setPassword('')
+    }
+
+    const handleShowPassword = () =>{
+        setShowPassword(!showPassword)
     }
   return (
     <div className="login-container">
@@ -25,19 +33,23 @@ const Login = () => {
         </div>
         <div className="form-group">
           <label className="form-label" htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            className="form-input"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
+            <input
+                type={showPassword ? "text" : "password"}
+                id="password"
+                className="form-input"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+            />
+            <span className='password-toggle-icon' onClick={handleShowPassword}>
+                {showPassword ? <FaEye /> : <FaEyeSlash/> }
+            </span>
         </div>
         <div>
             <a href="#" className="forgot-password">Forgot Password?</a>
         </div>
         <button type="submit" className="login-button">Login</button>
+        <p className="login-text">Don't have an account? <a href="#">Sign Up</a></p>
       </form>
     </div>
   )
