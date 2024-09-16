fd52f65a6b11b2baf118816fefe4a422bff1a108
Yashpal-vaghela <vaghela9632@gmail.com>
2024-09-16 14:42:21 +0530

    description added

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ const App = () => {
             <NewNav />
             <Allroutes />
             <Footer />
-            {/* <BackToTopButton /> */}
+            <BackToTopButton />
         </WatchlistProvider>
       </BrowserRouter>
     </>
