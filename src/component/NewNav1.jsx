fa3071ff5a511915944cd44bedce5dedc5fd4305
Yashpal-vaghela <vaghela9632@gmail.com>
2024-10-12 17:27:28 +0530

    Your commit message

--- src/component/NewNav1.jsx
+++ src/component/NewNav1.jsx
@@ -243,18 +243,6 @@ const NewNav1 = () => {
               )}
             </>
           )}
-
-          {/* {suggestions.length > 0 && (
-            <div className="suggestion-main">
-              <ul className="suggestions">
-                {suggestions.map((product) => (
-                  <li key={product.id} onClick={() => handleClick(product.id)}>
-                    {product.title}
-                  </li>
-                ))}
-              </ul>
-            </div>
-          )} */}
         </div>
         <div className="navbar-contact-details-wrapper">
           <span>
@@ -397,7 +385,6 @@ const NewNav1 = () => {
                     ? "navbar-link navbar-nav  mb-lg-0 navbar-show-icon"
                     : "navbar-link navbar-nav me-auto mb-2 mb-lg-0"
                 }`}
-                // className="navbar-link navbar-nav me-auto mb-2 mb-lg-0"
               >
                 <li className="nav-item" onClick={handleOffcanvas1}>
                   <Link to="/" className="nav-link active" aria-current="page">
@@ -492,22 +479,6 @@ const NewNav1 = () => {
                         </div>
                       </>
                     )}
-                    {/* {suggested.length > 0 ? (
-                      <div className="suggestion-sub">
-                        <ul className="suggested">
-                          {suggested.map((product) => (
-                            <li
-                              key={product.id}
-                              // onClick={() => handleClick(product.id)}
-                            >
-                              {product.title}
-                            </li>
-                          ))}
-                        </ul>
-                      </div>
-                    ) : (
-                      <></>
-                    )} */}
                   </>
                 ) : (
                   <></>
@@ -639,35 +610,3 @@ const NewNav1 = () => {
 };
 
 export default NewNav1;
-
-// {
-//   /* <div className="d-lg-none d-md-none d-sm-flex d-flex searchbar-wrapper1">
-//         <input
-//           className="form-control search-input"
-//           type="text"
-//           placeholder="Search..."
-//           value={searchQuery}
-//           onChange={handleSearchInputChangeSub}
-//         ></input>
-//         {searchQuery ? (
-//           <>
-//             {" "}
-//             <i className="fa-solid fa-xmark"></i>
-//           </>
-//         ) : (
-//           <>
-//             <svg
-//               stroke="currentColor"
-//               fill="currentColor"
-//               strokeWidth="0"
-//               viewBox="0 0 512 512"
-//               height="1em"
-//               width="1em"
-//               xmlns="http://www.w3.org/2000/svg"
-//             >
-//               <path d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path>
-//             </svg>
-//           </>
-//         )}
-//       </div> */
-// }
