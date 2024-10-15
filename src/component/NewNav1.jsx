7751a03282e95b8463871b5255cb9818ec82c71b
SavitriDhameliya <shavitridhameliya@gmail.com>
2024-10-15 08:41:33 +0530

    commit console in newnav

--- src/component/NewNav1.jsx
+++ src/component/NewNav1.jsx
@@ -10,7 +10,7 @@ import {
   FaPhoneAlt,
   FaTimes,
 } from "react-icons/fa";
-import { useSelector } from "react-redux";
+// import { useSelector } from "react-redux";
 import { useWatchlist } from "../Pages/WatchlistContext";
 import axios from "axios";
 
@@ -18,16 +18,16 @@ const NewNav1 = () => {
   const { watchlist ,cartList} = useWatchlist();
   // const cartData = useSelector((state) => state.cart.cartItems);
   const CartLength = localStorage.getItem("cart_length");
-  const WishList = localStorage.getItem("wishlist_length");
+  // const WishList = localStorage.getItem("wishlist_length");
   const [searchIcon, setSeachIcon] = useState(false);
   const [searchQuery, setSeachQuery] = useState("");
   const [suggested, setSuggested] = useState([]);
   const [suggestions, setSuggestions] = useState([]);
   const [loginData,setLoginData] = useState(localStorage.getItem('token'));
-  const [UserData] = useState(JSON.stringify(localStorage.getItem('UserData')))
+  // const [UserData] = useState(JSON.stringify(localStorage.getItem('UserData')))
   const getCartData = async () =>{
     const logindata = localStorage.getItem("token");
-    console.log("loginData",logindata,UserData)
+    // console.log("loginData",logindata,UserData)
     setLoginData(loginData)
     // if(loginData !== null){
     //   await axios.get(`https://admin.bossdentindia.com/wp-json/custom/v1/cart-items?user_id=${fetchuserdata.user_id}`)
@@ -551,9 +551,9 @@ const NewNav1 = () => {
                 <div className="cart-icon icon" onClick={handleOffcanvas1}>
                   <Link to="/cart">
                     <FaCartPlus />
-                    {
+                    {/* {
                       console.log("cartList",cartList,"loginData",loginData)
-                    }
+                    } */}
                     <span>{cartList?.length}</span>
                   </Link>
                 </div>
@@ -649,35 +649,3 @@ const NewNav1 = () => {
 };
 
 export default NewNav1;
-
-// {
-//   /* <div className="d-lg-none d-md-none d-sm-flex d-flex searchbar-wrapper1">
-//         <input
-//           className="form-control search-input"
-//           type="text"
-//           placeholder="Search..."
-//           value={searchQuery}
-//           onChange={handleSearchInputChangeSub}
-//         ></input>
-//         {searchQuery ? (
-//           <>
-//             {" "}
-//             <i className="fa-solid fa-xmark"></i>
-//           </>
-//         ) : (
-//           <>
-//             <svg
-//               stroke="currentColor"
-//               fill="currentColor"
-//               strokeWidth="0"
-//               viewBox="0 0 512 512"
-//               height="1em"
-//               width="1em"
-//               xmlns="http://www.w3.org/2000/svg"
-//             >
-//               <path d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path>
-//             </svg>
-//           </>
-//         )}
-//       </div> */
-// }
